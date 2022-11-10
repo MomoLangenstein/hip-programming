cd731e900cb92c424dbf6953260b1d88f650dee2
training141 Student <training141@puhti-login15.bullx>
2022-11-10 13:09:52 +0200

    1st stream exercise

--- streams/01-event-record/record.cpp
+++ streams/01-event-record/record.cpp
@@ -2,6 +2,14 @@
 #include <time.h>
 #include <hip/hip_runtime.h>
 
+#define HIP_ERRCHK(err) (hip_errchk(err, __FILE__, __LINE__ ))
+static inline void hip_errchk(hipError_t err, const char *file, int line) {
+  if (err != hipSuccess) {
+    printf("\n\n%s in %s at line %d\n", hipGetErrorString(err), file, line);
+    exit(EXIT_FAILURE);
+  }
+}
+
 /* A simple GPU kernel definition */
 __global__ void kernel(int *d_a, int n_total)
 {
@@ -12,7 +20,6 @@ __global__ void kernel(int *d_a, int n_total)
 
 /* The main function */
 int main(){
-  
   // Problem size
   constexpr int n_total = 4194304; // pow(2, 22);
 
@@ -23,31 +30,43 @@ int main(){
   // Allocate host and device memory
   int *a, *d_a;
   const int bytes = n_total * sizeof(int);
-  hipHostMalloc((void**)&a, bytes); // host pinned
-  hipMalloc((void**)&d_a, bytes);   // device pinned
+  HIP_ERRCHK(hipHostMalloc((void**)&a, bytes)); // host pinned
+  HIP_ERRCHK(hipMalloc((void**)&d_a, bytes));   // device pinned
 
   // Create events
-  #error create the required timing events here
+  hipEvent_t start_kernel_event;
+  hipEvent_t start_d2h_event;
+  hipEvent_t stop_event;
+
+  HIP_ERRCHK(hipEventCreate(&start_kernel_event));
+  HIP_ERRCHK(hipEventCreate(&start_d2h_event));
+  HIP_ERRCHK(hipEventCreate(&stop_event));
 
   // Create stream
   hipStream_t stream;
-  hipStreamCreate(&stream);
+  HIP_ERRCHK(hipStreamCreate(&stream));
 
   // Start timed GPU kernel and device-to-host copy
-  #error record the events somewhere across the below lines of code
-  #error such that you can get the timing for the kernel, the
-  #error memory copy, and the total combined time of these
+  HIP_ERRCHK(hipEventRecord(start_kernel_event, stream));
   clock_t start_kernel_clock = clock();
   kernel<<<gridsize, blocksize, 0, stream>>>(d_a, n_total);
 
+  HIP_ERRCHK(hipEventRecord(start_d2h_event, stream));
   clock_t start_d2h_clock = clock();
-  hipMemcpyAsync(a, d_a, bytes, hipMemcpyDeviceToHost, stream);
+  HIP_ERRCHK(hipMemcpyAsync(a, d_a, bytes, hipMemcpyDeviceToHost, stream));
 
+  HIP_ERRCHK(hipEventRecord(stop_event, stream));
   clock_t stop_clock = clock();
-  hipStreamSynchronize(stream);
+  HIP_ERRCHK(hipStreamSynchronize(stream));
 
   // Exctract elapsed timings from event recordings
-  #error get the elapsed time from the timing events
+  float kernel_time;
+  float d2h_time;
+  float total_time;
+
+  HIP_ERRCHK(hipEventElapsedTime(&kernel_time, start_kernel_event, start_d2h_event));
+  HIP_ERRCHK(hipEventElapsedTime(&d2h_time, start_d2h_event, stop_event));
+  HIP_ERRCHK(hipEventElapsedTime(&total_time, start_kernel_event, stop_event));
 
   // Check that the results are right
   int error = 0;
@@ -64,7 +83,9 @@ int main(){
 
   // Print event timings
   printf("Event timings:\n");
-  #error print event timings here
+  printf("  %.3f ms - kernel\n", kernel_time);
+  printf("  %.3f ms - device to host copy\n", d2h_time);
+  printf("  %.3f ms - total time\n", total_time);
 
   // Print clock timings
   printf("clock_t timings:\n");
@@ -73,12 +94,14 @@ int main(){
   printf("  %.3f ms - total time\n", 1e3 * (double)(stop_clock - start_kernel_clock) / CLOCKS_PER_SEC);
 
   // Destroy Stream
-  hipStreamDestroy(stream);
+  HIP_ERRCHK(hipStreamDestroy(stream));
 
   // Destroy events
-  #error destroy events here
+  HIP_ERRCHK(hipEventDestroy(start_kernel_event));
+  HIP_ERRCHK(hipEventDestroy(start_d2h_event));
+  HIP_ERRCHK(hipEventDestroy(stop_event));
 
   // Deallocations
-  hipFree(d_a); // Device
-  hipHostFree(a); // Host
+  HIP_ERRCHK(hipFree(d_a)); // Device
+  HIP_ERRCHK(hipHostFree(a)); // Host
 }
