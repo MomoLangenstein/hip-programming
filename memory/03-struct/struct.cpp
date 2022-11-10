6d58fcea09f31c0b88ae704a9327b5b9e1ca9b19
training141 Student <training141@puhti-login15.bullx>
2022-11-10 15:32:23 +0200

    3rd memory exercise

--- memory/03-struct/struct.cpp
+++ memory/03-struct/struct.cpp
@@ -57,9 +57,12 @@ void runHost()
 /* Run on device using Unified Memory */
 void runDeviceUnifiedMem()
 {
-  #error Allocate struct using Unified Memory
+  Example *ex;
+  hipMallocManaged(&ex, sizeof(Example));
+  ex->size = 10;
 
-  #error Allocate struct members using Unified Memory
+  hipMallocManaged(&ex->x, ex->size * sizeof(float));
+  hipMallocManaged(&ex->idx, ex->size * sizeof(int));
 
   // Initialize struct from host
   for(int i = 0; i < ex->size; i++)
@@ -68,42 +71,61 @@ void runDeviceUnifiedMem()
     ex->idx[i] = i;
   }
 
-  #error Print struct values from device by calling hipKernel()
   printf("\nDevice (UnifiedMem):\n");
+  hipKernel<<<1, ex->size>>>(ex);
 
-  #error Free struct
+  hipFree(ex->x);
+  hipFree(ex->idx);
+  hipFree(ex);
 }
 
 /* Create the device struct (needed for explicit memory management) */
 Example* createDeviceExample(Example *ex)
 {
-  #error Allocate device struct
+  Example *d_ex;
+  hipMalloc(&d_ex, sizeof(Example));
 
-  #error Allocate device struct members
+  float *d_x;
+  int *d_idx;
 
-  #error Copy arrays pointed by the struct members from host to device
+  hipMalloc(&d_x, ex->size * sizeof(float));
+  hipMalloc(&d_idx, ex->size * sizeof(int));
 
-  #error Copy struct members from host to device
+  hipMemcpy(d_x, ex->x, ex->size * sizeof(float), hipMemcpyHostToDevice);
+  hipMemcpy(d_idx, ex->idx, ex->size * sizeof(int), hipMemcpyHostToDevice);
 
-  #error Return device struct
+  hipMemcpy(&(d_ex->size), &(ex->size), sizeof(int), hipMemcpyHostToDevice);
+  hipMemcpy(&(d_ex->x), &d_x, sizeof(float*), hipMemcpyHostToDevice);
+  hipMemcpy(&(d_ex->idx), &d_idx, sizeof(int*), hipMemcpyHostToDevice);
+
+  return d_ex;
 }
 
 /* Free the device struct (needed for explicit memory management) */
 void freeDeviceExample(Example *d_ex)
 {
-  #error Copy struct members (pointers) from device to host
+  float *d_x;
+  float *d_idx;
 
-  #error Free device struct members
+  hipMemcpy(&d_x, &(d_ex->x), sizeof(float*), hipMemcpyDeviceToHost);
+  hipMemcpy(&d_idx, &(d_ex->idx), sizeof(int*), hipMemcpyDeviceToHost);
 
-  #error Free device struct
+  hipFree(d_x);
+  hipFree(d_idx);
+  hipFree(d_ex);
 }
 
 /* Run on device using Explicit memory management */
 void runDeviceExplicitMem()
 {
-  #error Allocate host struct
+  // Allocate host struct
+  Example *ex;
+  ex = (Example*)malloc(sizeof(Example));
+  ex->size = 10;
 
-  #error Allocate host struct members
+  // Allocate host struct members
+  ex->x = (float*)malloc(ex->size * sizeof(float));
+  ex->idx = (int*)malloc(ex->size * sizeof(int));
 
   // Initialize host struct
   for(int i = 0; i < ex->size; i++)
@@ -115,13 +137,16 @@ void runDeviceExplicitMem()
   // Allocate device struct and copy values from host to device
   Example *d_ex = createDeviceExample(ex);
 
-  #error Print struct values from device by calling hipKernel()
   printf("\nDevice (ExplicitMem):\n");
+  hipKernel<<<1, ex->size>>>(d_ex);
 
   // Free device struct
   freeDeviceExample(d_ex);
 
-  #error Free host struct
+  // Free host struct
+  free(ex->x);
+  free(ex->idx);
+  free(ex);
 }
 
 /* The main function */
